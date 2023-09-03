9ad9c197023c9db92c51689bed1573eb7e136077
Saptarshi Das <saptarshidas743@gmail.com>
2023-09-03 11:15:21 +0530

    Add GeeksforGeeks for problem Check if Tree is Isomorphic
    Added python solution for GeeksforGeeks problem Check if Tree is Isomorphic.

---
+++ geeksforgeeks/Check if Tree is Isomorphic/solution.py
@@ -0,0 +1,28 @@
+'''
+- Link to Problem: https://leetcode.com/problems/extra-characters-in-a-string/
+- Time Complexity: O(N**2) [N = min(nodes in root1, nodes in root2)]
+- Space Complexity: O(1)
+'''
+
+
+'''
+class Node:
+    def __init__(self, data=0):
+        self.data = data
+        self.left = None
+        self.right = None
+'''
+
+class Solution:
+    def isIsomorphic(self, root1, root2):
+        if not (root1 or root2):
+            return True
+        if not (root1 and root2) or root1.data != root2.data:
+            return False
+            
+        if self.isIsomorphic(root1.left, root2.left) and self.isIsomorphic(root1.right, root2.right):
+            return True
+        elif self.isIsomorphic(root1.left, root2.right) and self.isIsomorphic(root1.right, root2.left):
+            return True
+        
+        return False
