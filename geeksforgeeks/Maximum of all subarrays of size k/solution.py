de70fca9d08333112b1a3b77d3aeea54cb08accf
Saptarshi Das <saptarshidas743@gmail.com>
2023-11-11 13:06:22 +0530

    Solve GeeksforGeeks problem Maximum of all subarrays of size k
    Add python solution for GeeksforGeeks problem Maximum of all subarrays of size k.

---
+++ geeksforgeeks/Maximum of all subarrays of size k/solution.py
@@ -0,0 +1,28 @@
+'''
+- Link to Problem: https://www.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k3101/1
+- Time Complexity: O(N)
+- Space Complexity: O(K)
+'''
+
+from typing import List
+from collections import deque
+
+
+class Solution:
+    def max_of_subarrays(self, nums: List[int], n: int, k: int) -> List[int]:
+        queue = deque([])
+        ans = []
+        
+        for i in range(n):
+            while queue and queue[0] < i - k + 1:
+                queue.popleft()
+            
+            while queue and nums[queue[-1]] <= nums[i]:
+                queue.pop()
+            
+            queue.append(i)
+            
+            if i >= k - 1:
+                ans.append(nums[queue[0]])
+        
+        return ans
