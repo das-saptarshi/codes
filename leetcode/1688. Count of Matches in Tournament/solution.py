54fc36de66f78055183aea427d276f0cfa125bf0
Saptarshi Das <saptarshidas743@gmail.com>
2023-12-05 14:11:05 +0530

    Solve Leetcode problem 1688. Count of Matches in Tournament
    Add python solution for Leetcode problem 1688. Count of Matches in Tournament.

---
+++ leetcode/1688. Count of Matches in Tournament/solution.py
@@ -0,0 +1,9 @@
+'''
+- Link to Problem: https://leetcode.com/problems/count-of-matches-in-tournament/
+- Time Complexity: O(1)
+- Space Complexity: O(1)
+'''
+
+class Solution:
+    def numberOfMatches(self, n: int) -> int:
+        return n - 1
