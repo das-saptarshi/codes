6e1a860ccd2a841e2b924386eca7823c9f406659
Saptarshi Das <saptarshidas743@gmail.com>
2024-01-30 23:39:14 +0530

    Solve Leetcode problem 150. Evaluate Reverse Polish Notation
    Add python solution for Leetcode problem 150. Evaluate Reverse Polish Notation.

---
+++ leetcode/150. Evaluate Reverse Polish Notation/solution.py
@@ -0,0 +1,24 @@
+'''
+- Link to Problem: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+- Time Complexity: O(N)
+- Space Complexity: O(N)
+'''
+
+from typing import List
+
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for token in tokens:
+            if token.lstrip('-+').isnumeric():
+                stack.append(int(token))
+            else:
+                num2 = stack.pop()
+                num1 = stack.pop()
+
+                if token == '+': stack.append(num1 + num2)
+                elif token == '-': stack.append(num1 - num2)
+                elif token == '*': stack.append(num1 * num2)
+                elif token == '/': stack.append(int(num1 / num2))
+            
+        return stack.pop()
