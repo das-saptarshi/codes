d3d5cd67849eb5d9a9948defd0f0508ec0517e2b
Saptarshi Das <saptarshidas743@gmail.com>
2024-03-22 16:44:39 +0530

    Solve Leetcode problem 278. First Bad Version
    Add python solution for Leetcode problem 278. First Bad Version.

---
+++ leetcode/278. First Bad Version/solution.py
@@ -0,0 +1,23 @@
+'''
+- Link to Problem: https://leetcode.com/problems/first-bad-version/
+- Time Complexity: O(LogN)
+- Space Complexity: O(1)
+'''
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        left, right = 1, n
+
+        while left < right:
+            mid = left + (right - left) // 2
+
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+
+        return left
+    
+# The isBadVersion API is already defined for you.
+def isBadVersion(version: int) -> bool:
+    pass
