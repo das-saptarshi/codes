bc7b22ed465ad7758dd8a09f50bcdb706e04af78
Saptarshi Das <saptarshidas743@gmail.com>
2024-05-15 21:06:24 +0530

    Update solution for Leetcode problem 560. Subarray Sum Equals K
    Update solution implementation for Leetcode problem 560. Subarray Sum Equals K.

--- leetcode/560. Subarray Sum Equals K/solution.py
+++ leetcode/560. Subarray Sum Equals K/solution.py
@@ -5,19 +5,20 @@
 '''
 
 from typing import List
+from collections import defaultdict
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        
-        for i in range(1, len(nums)):
-            nums[i] += nums[i-1]
-        
-        prev = {}
-        ans = 0
+        prefix_sum_count = defaultdict(int)
+        prefix_sum = total = 0
+
         for num in nums:
-            if num == k:
-                ans += 1
-            if (num - k) in prev:
-                ans += prev[num-k]
-            prev[num] = prev.get(num, 0) + 1
-        return ans
+            prefix_sum += num
+
+            target = prefix_sum - k
+            if prefix_sum == k: total += 1
+            total += prefix_sum_count[target]
+ 
+            prefix_sum_count[prefix_sum] += 1
+        
+        return total
